6c2e8e15cc16023d428cfa9fbac40f57ab47f749
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2021-09-28 12:44:59 +0200

    reenable ksql unit tests

--- dashjoin-kafka/src/test/java/org/dashjoin/service/KsqlDBModelTest.java
+++ dashjoin-kafka/src/test/java/org/dashjoin/service/KsqlDBModelTest.java
@@ -1,8 +1,5 @@
 package org.dashjoin.service;
 
-import org.junit.jupiter.api.Disabled;
-
-@Disabled("DI errors - should not extend unit test classes")
 public class KsqlDBModelTest extends ModelTest {
 
 }
