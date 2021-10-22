aeab65e246989a59906f5ef9286e18825c54f948
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2021-10-22 14:45:09 +0200

    update build script

--- dashjoin-rdf4j/build.sh
+++ dashjoin-rdf4j/build.sh
@@ -1,3 +1,8 @@
+# make sure dashjoin-sdk is built
+cd ..
+mvn install
+cd dashjoin-rdf4j
+
 mvn install -Denforcer.skip
 docker build -t dashjoin/rdf4j .
 docker tag dashjoin/rdf4j:latest gcr.io/djfire-1946d/rdf4j
