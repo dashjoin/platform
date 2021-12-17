a3f2afa39dbad460e1f33b0175d7bc8935c6f539
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2021-12-17 19:26:42 +0100

    add unit tests

--- dashjoin-core/src/test/java/org/dashjoin/util/OpenCypherQueryTest.java
+++ dashjoin-core/src/test/java/org/dashjoin/util/OpenCypherQueryTest.java
@@ -1,10 +1,30 @@
 package org.dashjoin.util;
 
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.ws.rs.core.SecurityContext;
+import org.dashjoin.service.Data;
+import org.dashjoin.service.JSONDatabase;
+import org.dashjoin.service.Services;
 import org.dashjoin.util.OpenCypherQuery.Table;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.test.junit.QuarkusTest;
 
+@QuarkusTest
 public class OpenCypherQueryTest {
+
+  @Inject
+  Services services;
+
+  @Inject
+  Data data;
+
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   @Test
   public void testAggregate() throws Exception {
@@ -48,5 +68,50 @@ public class OpenCypherQueryTest {
   void edge(String s) {
     Table t = new Table(s, true);
     Assert.assertEquals(s, t.toString());
+  }
+
+  List<Map<String, Object>> run(String s) throws Exception {
+    SecurityContext sc = Mockito.mock(SecurityContext.class);
+    Mockito.when(sc.isUserInRole(Matchers.anyString())).thenReturn(true);
+    OpenCypherQuery q = new OpenCypherQuery(s);
+    return objectMapper.convertValue(q.run(services, data, sc, null), JSONDatabase.trTable);
+  }
+
+  @Test
+  public void testObjectsContainResource() throws Exception {
+    List<Map<String, Object>> res = run("MATCH (p:`dj/junit/EMP`) RETURN p");
+    Assert.assertEquals(2, res.size());
+    Assert.assertEquals(
+        "{ID=1, NAME=mike, WORKSON=1000, _dj_resource={database=junit, table=EMP, pk=[1]}}",
+        "" + res.get(0).get("p"));
+  }
+
+  @Test
+  public void testProjectField() throws Exception {
+    List<Map<String, Object>> res = run("MATCH (p:`dj/junit/EMP`) RETURN p.NAME, p.WORKSON");
+    Assert.assertEquals("{p.NAME=mike, p.WORKSON=1000}", "" + res.get(0));
+  }
+
+  @Test
+  public void testTraverse() throws Exception {
+    List<Map<String, Object>> res =
+        run("MATCH (p:`dj/junit/EMP`)-[e:WORKSON]->(project) RETURN p, e, project");
+    Assert.assertEquals("{_dj_edge=WORKSON, _dj_outbound=true}", "" + res.get(0).get("e"));
+    Assert.assertEquals("{ID=1000, NAME=dev-project}", "" + res.get(0).get("project"));
+  }
+
+  @Test
+  public void testPath() throws Exception {
+    List<Map<String, Object>> res =
+        run("MATCH path=(p:`dj/junit/EMP`)-[e:WORKSON]->(project) RETURN path");
+    System.out.println(res);
+    @SuppressWarnings("unchecked")
+    Map<String, Object> x = (Map<String, Object>) res.get(0).get("path");
+    Assert.assertEquals(
+        "{ID=1, NAME=mike, WORKSON=1000, _dj_resource={database=junit, table=EMP, pk=[1]}}",
+        "" + x.get("start"));
+    Assert.assertEquals(
+        "[{edge={_dj_edge=WORKSON, _dj_outbound=true}, end={ID=1000, NAME=dev-project}}]",
+        "" + x.get("steps"));
   }
 }
