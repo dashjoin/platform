dd975ed205a7d9fc23d26d821945e443b8c6737c
Uli <uli.walther@gmail.com>
2022-02-08 11:20:07 +0100

    Added comment to ServicesApplication. Task #112

--- dashjoin-core/src/main/java/org/dashjoin/service/ServicesApplication.java
+++ dashjoin-core/src/main/java/org/dashjoin/service/ServicesApplication.java
@@ -3,11 +3,19 @@ package org.dashjoin.service;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import lombok.extern.java.Log;
 
+/**
+ * 
+ * JAX-RS Application class.
+ * 
+ * ServicesApplication needs to be separated from Services, because since 2.x Quarkus does not
+ * allow @Inject into Application any more.
+ * 
+ * See: https://github.com/quarkusio/quarkus/pull/22440
+ * 
+ */
 @ApplicationScoped
 @ApplicationPath("/")
-@Log
 public class ServicesApplication extends Application {
-
+  // Intentionally empty. Functionality is in Services class
 }
