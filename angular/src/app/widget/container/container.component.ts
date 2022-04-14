9af44b0f405b09cc153400cea5ede386fc87f431
Uli <uli.walther@gmail.com>
2022-04-14 16:21:09 +0200

    Fix container display issue

--- angular/src/app/widget/container/container.component.ts
+++ angular/src/app/widget/container/container.component.ts
@@ -60,8 +60,6 @@ export class ContainerComponent extends DJBaseComponent implements OnInit {
       // Bit of a hack, but does the job...
       if (this.layout.hideframe)
         classList.remove('mat-card');
-      else
-        classList.add('mat-card');
     } catch (e) { console.warn(e); }
 
 
