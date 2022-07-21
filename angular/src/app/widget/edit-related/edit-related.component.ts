7ad0ab0264be0a4ca41fd1f2ff1192e927090061
Uli <uli.walther@gmail.com>
2022-07-21 11:21:37 +0200

    Base editRelated widget on DJData API. #6

--- angular/src/app/widget/edit-related/edit-related.component.ts
+++ angular/src/app/widget/edit-related/edit-related.component.ts
@@ -42,17 +42,28 @@ export class EditRelatedComponent extends LinksComponent implements OnInit {
         this.app.getSchema(parts[1], parts[2]).subscribe(res => this.createSchema = res);
       }
 
+      // set DB + table so that this.getData() works
+      this.database = encodeURIComponent(parts[1]);
+      this.table = encodeURIComponent(parts[2]);
+      // Note: getData() has special handling for config/Table, which we don't want here...
+      // This we set data manually:
+      this.data = 'dj/' + this.database + '/' + Util.encodeTableOrColumnName(this.table);
+
       // search key (where FK = this)
       const arg = {};
-      arg[parts[3]] = this.pk1;
+      arg[parts[3]] = (this.pk1);
 
-      this.http.post<object[]>('/rest/database/all/' + encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2]), arg, {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'x-dj-cache': encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2])
-        })
-      }).subscribe(res => {
-        this.all = res;
+      console.log('all', '/rest/database/all/' + encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2]), arg);
+
+      this.getData().get({ arguments: arg }).then(res => {
+        console.log('res', res.data)
+        // this.http.post<object[]>('/rest/database/all/' + encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2]), arg, {
+        //   headers: new HttpHeaders({
+        //     'Content-Type': 'application/json',
+        //     'x-dj-cache': encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2])
+        //   })
+        // }).subscribe(res => {
+        this.all = res.data;
         if (this.layout.columns) {
           this.columns = this.layout.columns;
         } else {
@@ -67,29 +78,44 @@ export class EditRelatedComponent extends LinksComponent implements OnInit {
   /**
    * computes the CRUD URL for the related entity (using the selected table row)
    */
-  relatedUrl(): string {
+  _relatedUrl(): string {
     let res = '/rest/database/crud/' + Util.parseTableID(this.queryMeta.ID)[1] + '/' + Util.parseTableID(this.queryMeta.ID)[2];
     for (const p of Object.values(this.queryMeta.properties)) {
       if (p.pkpos === 0) {
         res = res + '/' + encodeURIComponent(this.selected[p.name]);
       }
     }
+    console.log('relatedUrl', res, this.queryMeta);
     return res;
+  }
+
+  relatedKey(): string {
+    console.log('meta', this.queryMeta)
+    for (const p of Object.values(this.queryMeta.properties)) {
+      if (p.pkpos === 0) {
+        return encodeURIComponent(this.selected[p.name]);
+      }
+    }
+    throw new Error('Table has no primary key: ' + this.database + '/' + this.table);
   }
 
   /**
    * opens a popup allowing to edit a 1:n child
    */
   editRelated(): void {
+    const dbTable = this.database + '/' + this.table;
+    const key = this.relatedKey();
+
     const dialogRef = this.dialog.open(EditWidgetDialogComponent,
       { data: { value: this.selected, schema: this.queryMeta } });
     dialogRef.afterClosed().subscribe(data => {
       if (!data) { return; }
-      this.http.post(this.relatedUrl(), data, {
-        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-      }).subscribe(res => {
-        if (this.relatedUrl().startsWith('/rest/database/crud/config/Property/')) {
-          delete this.app.cache[decodeURIComponent(this.relatedUrl().substr('/rest/database/crud/config/Property/'.length))];
+      this.getData().update(key, data).then(res => {
+        // this.http.post(this.relatedUrl(), data, {
+        //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        // }).subscribe(res => {
+        if (dbTable === 'config/Property/') {
+          delete this.app.cache[decodeURIComponent(key)];
         }
         this.snackBar.open('Done', 'Ok', { duration: 3000 });
         this.eventChange.emit({ type: 'update' });
@@ -100,11 +126,13 @@ export class EditRelatedComponent extends LinksComponent implements OnInit {
   /**
    * create instance event handler
    */
-  create(onTable: string): void {
+  async create(onTable: string): Promise<void> {
     const parts = Util.parseTableOrColumnID(onTable);
-    this.http.put('/rest/database/crud/' + encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2]), this.createValue, {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }), responseType: 'text'
-    }).subscribe(res => {
+    console.log('create', parts)
+    this.getData().create(this.createValue).then(res => {
+    // this.http.put('/rest/database/crud/' + encodeURIComponent(parts[1]) + '/' + encodeURIComponent(parts[2]), this.createValue, {
+    //   headers: new HttpHeaders({ 'Content-Type': 'application/json' }), responseType: 'text'
+    // }).subscribe(res => {
       this.snackBar.open('Done', 'Ok', { duration: 3000 });
       if (res.startsWith('dj/config/Property/')) {
         res = decodeURIComponent(res.substr('dj/config/Property/'.length));
@@ -119,21 +147,26 @@ export class EditRelatedComponent extends LinksComponent implements OnInit {
   /**
    * delete instance event handler
    */
-  delete(url): void {
+  delete(): void {
+
+    const dbTable = this.database + '/' + this.table;
+
+    const key = this.relatedKey();
 
     // if we are deleting in an editRelated widget, it is an update, delete otherwise (triggers browser back)
     const type = this.layout.widget === 'edit' ? 'delete' : 'update';
 
     if (this.layout.deleteConfirmation) {
-      const confirm = (url.startsWith('/rest/database/crud/config/Table')) ||
-        (url.startsWith('/rest/database/crud/config/Property')) ? 'delete' : '';
+      const confirm = (dbTable === 'config/Table' ||
+        dbTable === 'config/Property') ? 'delete' : '';
       this.dialog.open(DeleteConfirmDialogComponent, {
         data: { display: this.layout.deleteConfirmation, confirm }
       }).afterClosed().subscribe(res => {
         if (res === confirm) {
-          this.http.delete(url).subscribe(res2 => {
-            if (url.startsWith('/rest/database/crud/config/Property/')) {
-              delete this.app.cache[decodeURIComponent(url.substr('/rest/database/crud/config/Property/'.length))];
+          this.getData().delete(key).then(res2 => {
+            //          this.http.delete(url).subscribe(res2 => {
+            if (dbTable === 'config/Property/') {
+              delete this.app.cache[decodeURIComponent(key)];
             }
             this.snackBar.open('Done', 'Ok', { duration: 3000 });
             this.eventChange.emit({ type });
@@ -141,7 +174,8 @@ export class EditRelatedComponent extends LinksComponent implements OnInit {
         }
       });
     } else {
-      this.http.delete(url).subscribe(res => {
+      this.getData().delete(key).then(res => {
+//        this.http.delete(url).subscribe(res => {
         this.snackBar.open('Done', 'Ok', { duration: 3000 });
         this.eventChange.emit({ type });
       }, this.errorHandler);
