fc874236696a5783f3b6dbdc8fce12c1fddc2059
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-03-12 17:47:51 +0100

    cache opt in for queries

--- dashjoin-core/src/test/java/org/dashjoin/service/ModelTest.java
+++ dashjoin-core/src/test/java/org/dashjoin/service/ModelTest.java
@@ -158,7 +158,7 @@ public class ModelTest {
           "if", "context", "properties", "deleteConfirmation", "print", "navigate", "markdown",
           "layout", "style", "class", /* schema info in config.json */ "name", "parent",
           /* gridster stuff */ "x", "y", "rows", "cols", /* redraw container */ "redrawInterval",
-          "expression", "html", "script", "hideframe", "clearCache", "card", "size")
+          "expression", "html", "script", "hideframe", "clearCache", "card", "size", "cached")
           .contains(e.getKey()));
       if (e.getKey().equals("url"))
         Assertions.assertTrue(e.getValue() instanceof TextNode);
