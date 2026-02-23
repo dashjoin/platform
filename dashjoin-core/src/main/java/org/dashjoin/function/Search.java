eacca8d8ab0509b9fda2c7655c8813bad7cc3164
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2026-02-23 12:16:48 +0100

    better error message for $search()

--- dashjoin-core/src/main/java/org/dashjoin/function/Search.java
+++ dashjoin-core/src/main/java/org/dashjoin/function/Search.java
@@ -9,17 +9,21 @@ public class Search extends AbstractVarArgFunction<List<SearchResult>> {
   @SuppressWarnings("rawtypes")
   @Override
   public List<SearchResult> run(List arg) throws Exception {
-    String term = (String) arg.get(0);
-    Integer limit = (Integer) arg.get(1);
-    String database = (String) arg.get(2);
-    String table = (String) arg.get(3);
-    if (term == null)
+    try {
+      String term = (String) arg.get(0);
+      Integer limit = (Integer) arg.get(1);
+      String database = (String) arg.get(2);
+      String table = (String) arg.get(3);
+      if (term == null)
+        throw new Exception("$search(term, limit?, database?, table?)");
+      if (database != null && table != null)
+        return this.expressionService.getData().search(this.sc, database, table, term, limit);
+      if (database != null)
+        return this.expressionService.getData().search(this.sc, database, term, limit);
+      return this.expressionService.getData().search(this.sc, term, limit);
+    } catch (ClassCastException e) {
       throw new Exception("$search(term, limit?, database?, table?)");
-    if (database != null && table != null)
-      return this.expressionService.getData().search(this.sc, database, table, term, limit);
-    if (database != null)
-      return this.expressionService.getData().search(this.sc, database, term, limit);
-    return this.expressionService.getData().search(this.sc, term, limit);
+    }
   }
 
   @Override
