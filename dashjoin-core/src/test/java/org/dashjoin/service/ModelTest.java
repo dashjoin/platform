e2a630722decc3ef00e2079d094a8622817c055e
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2026-03-30 22:16:04 +0200

    fix unit test

--- dashjoin-core/src/test/java/org/dashjoin/service/ModelTest.java
+++ dashjoin-core/src/test/java/org/dashjoin/service/ModelTest.java
@@ -153,17 +153,15 @@ public class ModelTest {
     }
 
     for (Entry<String, JsonNode> e : IteratorUtils.toList(tree.fields())) {
-      // System.out.println(e);
-      Assertions.assertTrue(Arrays
-          .asList("ID", "widget", "text", "title", "pageLayout", "readOnly", "tooltip", "icon",
-              "icons", "href", "children", "database", "table", "schema", "chart", "query",
-              "arguments", "fxHide", "display", "prop", "createSchema", "columns", "roles", "if",
-              "context", "properties", "deleteConfirmation", "print", "navigate", "markdown",
-              "layout", "style", "class", /* schema info in config.json */ "name", "parent",
-              /* gridster stuff */ "x", "y", "rows", "cols",
-              /* redraw container */ "redrawInterval", "expression", "html", "script", "hideframe",
-              "clearCache", "card", "size", "cached", "onChat", "uploadEnabled", "onRender")
-          .contains(e.getKey()));
+      System.out.println(e);
+      Assertions.assertTrue(Arrays.asList("ID", "widget", "text", "title", "pageLayout", "readOnly",
+          "tooltip", "icon", "icons", "href", "children", "database", "table", "schema", "chart",
+          "query", "arguments", "fxHide", "display", "prop", "createSchema", "columns", "roles",
+          "if", "context", "properties", "deleteConfirmation", "print", "navigate", "markdown",
+          "layout", "style", "class", /* schema info in config.json */ "name", "parent",
+          /* gridster stuff */ "x", "y", "rows", "cols", /* redraw container */ "redrawInterval",
+          "expression", "html", "script", "hideframe", "clearCache", "card", "size", "cached",
+          "onChat", "uploadEnabled", "onRender", "hideColumns", "foreach").contains(e.getKey()));
       if (e.getKey().equals("url"))
         Assertions.assertTrue(e.getValue() instanceof TextNode);
     }
